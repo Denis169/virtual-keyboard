dc1d0dd4920850c88b846294e8a9589f75b5186e
Denis <denis6125038@gmail.com>
2022-05-02 22:09:17 +0300

    feat: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Virtual keyboard',
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+    port: 3005,
+  },
+};
